f162945c2ba5c49c2b05323700a9d6e87859fd9b
Aadit M Shah <aaditmshah@fastmail.fm>
2014-03-16 07:17:20 +0530

    Added publi.sh.

---
+++ publi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $2 | openssl aes-256-cbc -salt -in src/Problem$1.hs -out src/Problem$1.hs.secret -pass stdin
+
+printf "\n$1. $2" >> answers.md
+
+openssl aes-256-cbc -salt -in answers.md -out answers.md.secret
+
+if [ $? -ne 0 ]; then
+    git checkout -- answers.md
+    exit $error
+fi
+
+sed -r s/#[0-9]+/#$1/ README.md > README.md
+
+git add README.md answers.md.secret src/Problem$1.hs.secret
+
+git commit -m "Solved Problem $1."
+
+git push -u origin master
